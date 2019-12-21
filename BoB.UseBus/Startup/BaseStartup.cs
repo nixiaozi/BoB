d7ff39c142e90ad8cc33eca0e67facf33a3565d1
lee cheng <nixiaozi01@hotmail.com>
2019-12-21 10:48:54 +0800

    BoB.TestApi\Program.cs Edit 解决autofac服务提供程序报错的问题

--- BoB.UseBus/Startup/BaseStartup.cs
+++ BoB.UseBus/Startup/BaseStartup.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using BoB.BaseModule;
+﻿using Autofac;
 using BoB.UseBus.Register;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 

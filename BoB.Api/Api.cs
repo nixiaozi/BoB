d1c540cc183543a172dcc156cc637c3d7fe2139d
lee cheng <nixiaozi01@hotmail.com>
2020-01-17 22:31:52 +0800

    全局API调用方法

---
+++ BoB.Api/Api.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BoB.Api
+{
+    public static class Api
+    {
+        public static ApiResult<K> DoApi<T, K>(T Data, Func<T, ApiResult<K>> DoSomething)
+            where T : IApiInput where K : IApiInput, IApiResult
+        {
+            ApiResult<K> result = new ApiResult<K>(false);
+            result = DoSomething(Data);
+            return result;
+        }
+
+
+
+
+
+    }
+}
---
+++ BoB.Api/IApiResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoB.Api
+{
+    public interface IApiResult
+    {
+
+    }
+}

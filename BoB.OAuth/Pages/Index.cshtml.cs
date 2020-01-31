3d41fc5804416cbb6b60ff137a0f7ce3dc3fdd8e
lee cheng <nixiaozi01@hotmail.com>
2020-01-31 20:32:18 +0800

    初始化添加开放是登录验证的项目

---
+++ BoB.OAuth/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BoB.BoBLogger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BoB.OAuth.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IBoBLogService _log;
+
+        public IndexModel(IBoBLogService log)
+        {
+            _log = log; 
+        }
+
+        public IActionResult OnGet()
+        {
+            _log.Error("Gsdge", "sdg", "gdsger");
+            return Content("something");
+        }
+    }
+}
